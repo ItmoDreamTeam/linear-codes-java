6ba51b0238331a89dc76a0698ed34af781f33d67
Kirill Smirnov <smirnowku@gmail.com>
2019-10-30 13:53:13 +0300

    Implement iterative LDPC decoding

--- src/main/java/linearcodes/LdpcCode.kt
+++ src/main/java/linearcodes/LdpcCode.kt
@@ -26,6 +26,7 @@ class LdpcCode : BlockCode {
 
     private val k = generatorMatrix.height
     private val n = generatorMatrix.width
+    private val r = n - k
 
     override fun encodeBlock(input: String): String {
         require(input.length == k) { "Input block size must be $k" }
@@ -39,10 +40,30 @@ class LdpcCode : BlockCode {
         val inputDigits = Utils.stringToList(input)
         val syndrome = BinaryMatrix.ofBytes(listOf(inputDigits)).mult(checkMatrix.transpose())
         val outputDigits = if (syndrome.isZero()) inputDigits else fixBlock(inputDigits)
-        return Utils.listToString(outputDigits.subList(0, 5))
+        return Utils.listToString(outputDigits.subList(0, k))
     }
 
     private fun fixBlock(block: List<Byte>): List<Byte> {
-        return block;
+        val fixingBlock = block.toMutableList()
+        for (i in 0..100) {
+            val syndrome = BinaryMatrix.ofBytes(listOf(fixingBlock)).mult(checkMatrix.transpose())
+            if (syndrome.isZero()) return fixingBlock
+            val incorrectBitIndex = computeIndexOfMostLikelyIncorrectBit(fixingBlock)
+            fixingBlock[incorrectBitIndex] = if (fixingBlock[incorrectBitIndex].toInt() == 0) 1 else 0
+        }
+        throw Exception("Received block contains erasures")
+    }
+
+    private fun computeIndexOfMostLikelyIncorrectBit(input: List<Byte>): Int {
+        val failedChecks = IntArray(n)
+        for (checkBitIndex in 0 until r) {
+            val mask = checkMatrix.content[checkBitIndex].toIntArray()
+            val parity = mask.zip(input, Int::times).sum() % 2
+            if (parity != 0) {
+                val controlledBitsIndices = mask.withIndex().filter { it.value == 1 }.map { it.index }
+                controlledBitsIndices.forEach { failedChecks[it]++ }
+            }
+        }
+        return failedChecks.indexOfFirst { it == failedChecks.max() }
     }
 }
